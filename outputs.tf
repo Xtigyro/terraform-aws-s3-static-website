e3949df02ab3fe23cea0e148db7321659aaf5625
Alex Hodges <ahodges22@gmail.com>
2019-03-16 19:16:45 -0700

    add cloudfront arn to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "cdn_domain_name" {
   value       = "${aws_cloudfront_distribution.cdn.domain_name}"
 }
 
+output "cdn_domain_arn" {
+  description = "Domain name of the Cloudfront Distribution"
+  value       = "${aws_cloudfront_distribution.cdn.arn}"
+}
+
 output "s3_bucket_arn" {
   value = "${aws_s3_bucket.static_website.arn}"
 }
